31bdd724bd449e32ee24365915285bf9b33925fa
Asakura23 <hpghpggg@gmail.com>
2022-08-21 17:54:52 +0300

    HomeWork#2

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require('src/functions.phtml');
+$car = array("model","speed","doors","year");
+
+echo task1($car, true)."<br>";
+
+task2("+",2,3,4,7,92,214,2.32);
+
+task3(10,5);
+
+task4();
+
+task5();
+
+createFile();
+task6("test.txt");
+
